91c115de6d400cbfc8acd8ab9f6e8f027ce5d60b
pepo <pepo.ospina@gmail.com>
2025-10-15 19:58:16 +0200

    Initial commit for payif-lib

---
+++ src/types/types.sessions.ts
@@ -0,0 +1,41 @@
+import type { Hex } from "viem";
+
+
+export interface CheckoutSessionItem {
+  name: string;
+  amount: number;
+  pricePerUnit: number;
+}
+
+export interface StreetAddress {
+  country: string;
+  state: string;
+  city: string;
+  street: string;
+  zipCode: string;
+}
+
+export interface Order {
+  currency: "USD" | "EUR";
+  items: CheckoutSessionItem[];
+  deliveryAddress: StreetAddress;
+  deliveryDate: number;
+  deliveryCarrier: string;
+}
+
+export interface CheckoutSession {
+  appId: string;
+  order: Order;
+  recipient: Hex;
+  redirectUrl: string;
+}
+
+
+export type CheckoutSessionCreate = {
+  session: CheckoutSession;
+  templateId?: string;
+};
+
+export type CheckoutSessionCreateResult = {
+  url: string;
+};
