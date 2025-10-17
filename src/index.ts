cc8a06fd5c834955bfbc33e5f14f03ec2681da99
pepo <pepo.ospina@gmail.com>
2025-10-17 12:19:12 +0200

    export non types

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { PayifClient } from "./client";
 export type { PayifClientConfig } from "./client";
 export type * from "./types/types.sessions";
 export type * from "./types/types.messages";
+export * from "./types/types.messages";
